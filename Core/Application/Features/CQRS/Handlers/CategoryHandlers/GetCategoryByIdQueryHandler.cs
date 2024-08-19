e2cb2c9bb89973b9de4213a377eee2a845026dbe
saidfaruk <faruk.said@hotmail.com>
2024-08-19 22:30:06 +0300

    KategoriUpdate

--- Core/Application/Features/CQRS/Handlers/CategoryHandlers/GetCategoryByIdQueryHandler.cs
+++ Core/Application/Features/CQRS/Handlers/CategoryHandlers/GetCategoryByIdQueryHandler.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Application.Features.CQRS.Queries.CarQueries;
+using Application.Features.CQRS.Queries.CategoryQueries;
+using Application.Features.CQRS.Results.CarResults;
+using Application.Features.CQRS.Results.CategoryResults;
+using Application.Interfaces;
+using CarBookDomain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +16,23 @@ namespace Application.Features.CQRS.Handlers.CategoryHandlers
     {
 
 
+        private readonly IRepository<Category> _repoistory;
+
+        public GetCategoryByIdQueryHandler(IRepository<Category> repoistory)
+        {
+            _repoistory = repoistory;
+        }
+
+
+        public async Task<GetCategoryByIdQueryResult> Handle(GetCategoryByIdQuery query)
+        {
+            var values = await _repoistory.GetByIdAsync(query.Id);
+            return new GetCategoryByIdQueryResult
+            {
+                CategoryID = values.CategoryID,
+                Name = values.Name,
+
+            };
+        }
     }
 }
---
+++ Presentation/Web.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,82 @@
+﻿using Application.Features.CQRS.Commands.CategoryCommands;
+using Application.Features.CQRS.Handlers.CategoryHandlers;
+using Application.Features.CQRS.Queries.CategoryQueries;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+
+
+        private readonly CreateCategoryCommandHandler _createCommandHandler;
+        private readonly GetCategoryByIdQueryHandler _getCategoryByIdQueryHandler;
+        private readonly GetCategoryQueryHandler _getCategoryQueryHandler;
+        private readonly UpdateCategoryCommandHandler _updateCategoryCommandHandler;
+        private readonly RemoveCategoryCommandHandler _removeCategoryCommandHandler;
+
+        public CategoriesController(CreateCategoryCommandHandler createCommandHandler, GetCategoryByIdQueryHandler getCategoryByIdQueryHandler, GetCategoryQueryHandler getCategoryQueryHandler, UpdateCategoryCommandHandler updateCategoryCommandHandler, RemoveCategoryCommandHandler removeCategoryCommandHandler)
+        {
+            _createCommandHandler = createCommandHandler;
+            _getCategoryByIdQueryHandler = getCategoryByIdQueryHandler;
+            _getCategoryQueryHandler = getCategoryQueryHandler;
+            _updateCategoryCommandHandler = updateCategoryCommandHandler;
+            _removeCategoryCommandHandler = removeCategoryCommandHandler;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> CategoryList()
+        {
+            var values = await _getCategoryQueryHandler.Handle();
+            return Ok(values);
+        }
+
+
+        [HttpGet("{id}")]
+
+
+        public async Task<IActionResult> GetCategoryById(int id)
+        {
+            var values = await _getCategoryByIdQueryHandler.Handle(new GetCategoryByIdQuery(id));
+            return Ok(values);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryCommand command)
+        {
+            await _createCommandHandler.Handle(command);
+            return Ok("Kategori  bilgisi eklendi");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> RemoveCategory(int id)
+        {
+            await _removeCategoryCommandHandler.Handle(new RemoveCategoryCommand(id));
+            return Ok("silme islemi basarili");
+        }
+
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryCommand command)
+        {
+            await _updateCategoryCommandHandler.Handle(command);
+            return Ok("Kategori bilgisi guncellendi");
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
